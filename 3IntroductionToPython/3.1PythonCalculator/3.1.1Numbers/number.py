2817c409a8c63d449837aea5176af93309c11e2e
sumaiya-mimma <samimma958@gmail.com>
2023-10-10 17:08:58 +0600

    how to do basic calculation

--- 3IntroductionToPython/3.1PythonCalculator/3.1.1Numbers/number.py
+++ 3IntroductionToPython/3.1PythonCalculator/3.1.1Numbers/number.py
@@ -1,9 +1,30 @@
-msg = "Hello world"
+a=1;
+a=2;
+print("this is line number 3 :", a);
+a=3;
+a=4;
+print("this is line number 6 :",a);
 
-print(msg)
+# operator : + - * /
 
-a=10;
-print(a)
+print((10+200*2)/2)
 
-a= "python"
-print(a)
+# divide operator always returs a float number
+print(10/5);
+print(10/3)
+print(5/5)
+# to get floor number we need to use (//)
+print(10//3)
+# to get reminder we need to use (%)
+print(10%3)
+
+
+a = 10;
+b = 3;
+result = a*b;
+print(result);
+
+print(result/2);
+
+
+
