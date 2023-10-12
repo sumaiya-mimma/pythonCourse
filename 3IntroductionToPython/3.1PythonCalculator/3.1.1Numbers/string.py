6e839482a34c9130acb8f10f8b91e2c0a6189e96
sumaiya-mimma <samimma958@gmail.com>
2023-10-12 16:33:05 +0600

    introduction to string

---
+++ 3IntroductionToPython/3.1PythonCalculator/3.1.1Numbers/string.py
@@ -0,0 +1,39 @@
+# Text, characters "" string
+
+a = 10;
+b = "Hi";
+c = 'MOM';
+
+#print(a,b,c);
+
+d = "20";
+#print(d/2);
+
+
+msg = "Today is Thursday!"
+#print(msg);
+msg = "Today 'is' Thursday";
+#print(msg);
+msg3 = 'Today "is" Thursday';
+
+# print(msg3);
+
+# msg4= "dosn't"
+# print(msg4);
+
+# msg5= "don't worry"
+# print(msg5)
+
+# msg6= "DON'T WORRY"
+# print(msg6)
+
+# msg7= "don't'"
+# print(msg7)
+
+
+txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n sed do eiusmod tempor\t incididuntut labore et dolore magna aliqua."
+
+print(txt);
+
+txt = "!hello, world,\n ********\n   ****\n     ***\n      **\n       *"
+print(txt)
