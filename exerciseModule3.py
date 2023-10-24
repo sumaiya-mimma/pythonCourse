e2a7a4fc07083e3b8981d0fe7973cb8e2e74f2d8
sumaiya-mimma <samimma958@gmail.com>
2023-10-24 14:25:24 +0600

    exercise 24,13,10,8 and 9

--- exerciseModule3.py
+++ exerciseModule3.py
@@ -21,25 +21,83 @@
 # if("number 1" == "number 2" == "number 3"):
 #     print(("number 1" + "number 2" + "number 3")*3)
     
+#  #18   
     
+# number1 = int(input("enter a number : "))
+# print(number1)
+# number2 = int(input("enter a number : "))
+# print(number2)
+# number3 = int(input("enter a number : "))
+# print(number3)
+# if(number1 == number2== number3):
+#     print("values are equal")
+#     print((number1 + number2 + number3)*3)
+# elif(number1 != number2 != number3):
+#     print("values are not equal")
+# else:
+#     print("not equal")
+
+
     
-number1 = int(input("enter a number : "))
-print(number1)
-number2 = int(input("enter a number : "))
-print(number2)
-number3 = int(input("enter a number : "))
-print(number3)
-if(number1 == number2== number3):
-    print("values are equal")
-    print((number1 + number2 + number3)*3)
-elif(number1 != number2 != number3):
-    print("values are not equal")
+#14
+# date1 = (2014,7,11)
+# date2 = (2014,7,2)
+# days = date1 - date2
+# print(days)
+# from datetime import days
+# date1 = (2014,7,11)
+# date2 = (2014,7,2)
+# delta = date1 - date2
+# print(delta.days)
+
+#10
+# n = 5
+# print(n+n*n+n*n*n)
+
+#13
+# print("""a string that you \"don\'t\" \nthis \nis a........multi-line  \nheredoc string-----------> example""")
+
+# #8
+# myList = ["red","green","white","black"]
+# print(myList[0])
+# print(myList[-1])
+
+
+
+#7
+# fileName= input("enter a file name : ")
+# extension = fileName.split(".")[-1]
+# print(extension)
+
+#9
+# exam_st_date = (11,12,2014)
+# print("the examination will start from:" %exam_st_date)
+
+#12
+# import calendar
+# print(calendar
+
+# year = int(input("enter an year : "))
+# print(year)
+# def printyear(calender):
+
+
+#22
+
+# arr = []
+# arrLen = int(input("how many numbers are in your list : "))
+# for i in range(arrLen):
+#     print("enter element number", i+1,":")
+#     element=int(input())
+#     arr.append(element)
+#     i = i+1
+# print(arr)
+# if("arr", has element, "4"):
+    
+
+#24
+userInput = (input("enter a letter:"))
+if(userInput == "vowel"):
+    print(userInput, "is a vowel")
 else:
-    print("not equal")
-
-
-    
-    
-
-
-
+    print(userInput, "is not a vowel")
