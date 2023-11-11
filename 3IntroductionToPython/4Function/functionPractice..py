9fdaa0e72bdf01d7ba6862b52a1df1d5108fbe57
sumaiya-mimma <samimma958@gmail.com>
2023-11-11 15:20:14 +0600

    list

---
+++ 3IntroductionToPython/4Function/functionPractice..py
@@ -0,0 +1,78 @@
+# my1DList = ["a",5,"b",6,"c",8]
+# my2DList = [["an",5],["b",[8,9,10]],["c",8]]
+
+# print(my2DList[0][0][1])
+
+
+# myStr = "institute"
+# print(myStr[2])
+
+# print(my2DList[2],',', my2DList[0])
+
+# otherVariable = [my1DList[4],my1DList[5]]
+# print(otherVariable)
+
+
+
+# numbers = [20,21,100,23,240, 50]
+# # print(len(numbers))
+
+# largeNumber = 0;
+# for i in range(len(numbers)):
+#     # print(numbers[i])
+#     if(numbers[i]>largeNumber):
+#         largeNumber = numbers[i]
+        
+# print(largeNumber)
+
+
+
+
+# my2DList = [["x",40], ["y",50,60], ["z",23]]
+
+# largeNumber = 0
+# for i in range(len(my2DList)):
+#     # print(my2DList[i][1])
+#     if((my2DList[i][1]>largeNumber)):
+#         largeNumber = my2DList[i][1]
+
+
+# print(largeNumber)
+
+
+# for j in range(3):    
+#     nums = [1, 2, 5, 6, 8]
+#     userInput = int(input("enter a number : "))
+#     print(userInput)
+
+#     flag = 0
+#     for i in range(len(nums)):
+#         if(userInput == nums[i]):
+#             flag = 100
+            
+#     if(flag == 100):
+#         print("yes")
+#     else:
+#         print("no")
+
+
+# my2DList = [["x",40], ["y",50], ["z",23]]
+
+# largeNumber = 0
+# for i in range(len(my2DList)):
+#     # print(my2DList[i][1])
+#     if((my2DList[i][1]>largeNumber)):
+#         largeNumber = my2DList[i][1]
+
+
+# print(largeNumber)
+
+
+my2DList = [["n",60],["m",50],["o",90]]
+
+largeNumber = 0
+for i in range(len(my2DList)):
+    if((my2DList[i][1])>largeNumber):
+        largeNumber = my2DList[i][1]
+        
+print(largeNumber)
