fc53c63e8789c54c34b2bb076c7dd35f58daeaa5
sumaiya-mimma <samimma958@gmail.com>
2023-11-13 22:53:36 +0600

    case

--- problems/Problems2/problems2.py
+++ problems/Problems2/problems2.py
@@ -1,11 +1,11 @@
-# x = input("Enter : ")
-# match x:
-#      case '1':
-#         print('Hello to you too!')
-#      case '2':
-#          print('See you later')
-#      case other:
-#          print('No match found')
+x = input("Enter : ")
+match x:
+     case '1':
+        print('Hello to you too!')
+     case '2':
+         print('See you later')
+     case other:
+         print('No match found')
 
 # mainBalance = 1600
 # i=0
@@ -61,72 +61,135 @@
 #             case other:
 #                 print("Please enter correct number.")
 #         newBalance = deposite(mainBalance)
-#         # return newBalance
+#         return newBalance
                 
-#     mainBalance =  newBalance
+#     # mainBalance =  newBalance
 #     initialPage()  
     
    
     # mainBalance = newBalance
     
     
-mainBalance = 1600
+# Balance = 1600
 # i=0
 # while i<10:
     
+#     mainbalance = ()
 
-def deposit(mainBalance):
-        amount  = int(input("enter an amount (TAKA): "))
-        previousBalance = mainBalance
-        mainBalance = mainBalance + amount
-        print("Deposit amount         : ", amount)
-        print("Your Prervious Balance : ", previousBalance)
-        print("Your  Current Balance  : ", mainBalance)
-        newBalance = mainBalance
-        return newBalance
+#     def deposit(Balance):
+#         amount  = int(input("enter an amount (TAKA): "))
+#         previousBalance = Balance
+#         mainBalance = Balance + amount
+#         print("Deposit amount         : ", amount)
+#         print("Your Prervious Balance : ", previousBalance)
+#         print("Your  Current Balance  : ", mainBalance)
+#         newBalance = mainBalance
+#         return newBalance
         
     
 
     
-def withdraw(mainBalance):
-        amount = int(input("enter an amount (TAKA): "))
-        previousBalance = mainBalance
-        if(amount<mainBalance):
-             mainBalance = mainBalance - amount
-        print("withdrawn amount         : ", amount)
-        print("Your Prervious Balance : ", previousBalance)
-        print("Your  Current Balance  : ", mainBalance)
-        newBalance = mainBalance
-        return newBalance
+#     def withdraw(Balance):
+#         amount = int(input("enter an amount (TAKA): "))
+#         previousBalance = Balance
+#         if(amount<Balance):
+#              mainBalance = Balance - amount
+#         print("withdrawn amount         : ", amount)
+#         print("Your Prervious Balance : ", previousBalance)
+#         print("Your  Current Balance  : ", mainBalance)
+#         newBalance = mainBalance
+#         return newBalance
     
-def initialPage():
-        print("""
-                .-----------------------------------------------.
-                |              MAYER DOWYA ATM                  |
-                | 1. check balance                              |
-                | 2. deposit                                    |
-                | 3. withdraw                                   |
-                |_______________________________________________|
+#     def initialPage():
+#         print("""
+#                 .-----------------------------------------------.
+#                 |              MAYER DOWYA ATM                  |
+#                 | 1. check balance                              |
+#                 | 2. deposit                                    |
+#                 | 3. withdraw                                   |
+#                 |_______________________________________________|
                 
-                """)
+#                 """)
         
-        selectedOption = input("choose your option(example: 2):")
-        match selectedOption:
-            case '1':
-                print("your balance is: ",mainBalance, "TAKA")
-            case '2':
-                # amount  = int(input("enter an amount (TAKA): "))
-                deposit(mainBalance)
-                newBalance = mainBalance
+#         selectedOption = input("choose your option(example: 2):")
+#         match selectedOption:
+#             case '1':
+#                 print("your balance is: ",Balance, "TAKA")
+#             case '2':
+#                 # amount  = int(input("enter an amount (TAKA): "))
+#                 deposit(Balance)
+#                 newBalance = mainBalance
                 
-            case '3':
-                # amount  = int(input("enter an amount (TAKA): "))
-                withdraw(mainBalance)
-                newBalance = mainBalance
-            case other:
-                print("Please enter correct number.")
-        newBalance = deposit(mainBalance)
-        return newBalance
+#             case '3':
+#                 # amount  = int(input("enter an amount (TAKA): "))
+#                 withdraw(Balance)
+#                 newBalance = mainBalance
+#             case other:
+#                 print("Please enter correct number.")
+#         newBalance = deposit(Balance)
+#         return newBalance
                 
-# mainBalance =  newBalance
-initialPage()  
+# # mainBalance =  newBalance
+#     initialPage()  
+
+
+
+
+# mainBalance = 1600
+# i = 0
+# while i<10:
+
+#  def deposit():
+#     amount = int(input("Enter an amount (TAKA): "))
+#     previousBalance = mainBalance
+#     mainBalance = mainBalance + amount
+#     print("Deposit amount         : ", amount)
+#     print("Your Previous Balance : ", previousBalance)
+#     print("Your Current Balance  : ", mainBalance)
+#     newBalance = mainBalance
+#     return newBalance
+
+#  def withdraw(mainBalance):
+#     amount = int(input("Enter an amount (TAKA): "))
+#     previousBalance = mainBalance
+#     if amount < mainBalance:
+#         mainBalance = mainBalance - amount
+#         print("Withdrawn amount       : ", amount)
+#         print("Your Previous Balance : ", previousBalance)
+#         print("Your Current Balance  : ", mainBalance)
+#         newBalance = mainBalance
+#         return newBalance
+
+#  def updatedBalance(mainBalance):
+    
+#  def initialPage(mainBalance):
+#     print("""
+#             .-----------------------------------------------.
+#             |              MAYER DOWYA ATM                  |
+#             | 1. Check balance                              |
+#             | 2. Deposit                                    |
+#             | 3. Withdraw                                   |
+#             |_______________________________________________|
+            
+#             .-----------------------------------------------.
+#             |              MAYER DOWYA ATM                  |
+#             | 4.New Balance                                 |
+#             | 5.Exit                                        |
+#             |_______________________________________________|
+#             """)
+    
+#     selectedOption = input("Choose your option (example: 2): ")
+#     match selectedOption:
+        
+#         case '1':
+#             print("your main balance is :", mainBalance, "taka" )
+#         case '2':
+#             deposit(mainBalance)
+#             newBalance = mainBalance
+#         case '3':
+#             withdraw(mainBalance)
+#             newBalance = mainBalance
+            
+#             return newBalance
+# initialPage()
+
