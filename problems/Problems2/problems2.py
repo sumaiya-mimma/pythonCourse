c5cb3778096387e5edb2e9a8d72dc122c817890c
sumaiya-mimma <samimma958@gmail.com>
2023-11-14 17:53:55 +0600

    atm problem solved

--- problems/Problems2/problems2.py
+++ problems/Problems2/problems2.py
@@ -1,195 +1,114 @@
-x = input("Enter : ")
-match x:
-     case '1':
-        print('Hello to you too!')
-     case '2':
-         print('See you later')
-     case other:
-         print('No match found')
+                # x = input("Enter : ")
+                # match x:
+                #      case '1':
+                #         print('Hello to you too!')
+                #      case '2':
+                #          print('See you later')
+                #      case other:
+                #          print('No match found')
+
+
+
+# userDB = [
+#     ['111', 1111, 1000],
+#     ['222', 2222, 2000],
+#     ['333', 3333, 3000],
+#     ['444', 4444, 4000],
+#     ['555', 5555, 5000],
+# ]
+
+userDB = [1,2,3,4,5,6]
+userCardNumber = int(input("Enter card your card Number : "))
+
+flag = 0
+for i in range(len(userDB)):
+    if((userDB[i])==userCardNumber):
+        print("yes")
+        flag = 1
+        break  
+if(flag==0):
+    print("No")
+        
+
 
 # mainBalance = 1600
-# i=0
-# while i<10:
-    
-#     def deposite(mainBalance):
-#         amount  = int(input("enter an amount (TAKA): "))
+
+# userPass = "1234"
+
+# def deposite(mainBalance):
+#     amount  = int(input("enter an amount (TAKA): "))
+#     inputPass = input("enter your PIN : ")
+#     if(inputPass == userPass):
 #         previousBalance = mainBalance
 #         mainBalance = mainBalance + amount
 #         print("Deposit amount         : ", amount)
 #         print("Your Prervious Balance : ", previousBalance)
 #         print("Your  Current Balance  : ", mainBalance)
-#         newBalance = mainBalance
-#         return newBalance
-        
+#         return mainBalance
+#     else:
+#         print("Wrong PIN! Please try again.")
+#         return mainBalance
     
 
-    
-#     def withdraw(mainBalance):
-#         amount = int(input("enter an amount (TAKA): "))
+
+
+# def withdraw(mainBalance):
+#     amount = int(input("enter an amount (TAKA) : "))
+#     inputPass = input("enter your PIN : ")
+#     if(inputPass == userPass):
 #         previousBalance = mainBalance
 #         if(amount<mainBalance):
-#              mainBalance = mainBalance - amount
-#         print("withdrawn amount         : ", amount)
-#         print("Your Prervious Balance : ", previousBalance)
-#         print("Your  Current Balance  : ", mainBalance)
-#         newBalance = mainBalance
-#         return newBalance
-    
-#     def initialPage():
-#         print("""
-#                 .-----------------------------------------------.
-#                 |              MAYER DOWYA ATM                  |
-#                 | 1. check balance                              |
-#                 | 2. deposite                                   |
-#                 | 3. withdraw                                   |
-#                 |_______________________________________________|
-                
-#                 """)
+#             mainBalance = mainBalance - amount
+#             print("withdrawn amount         : ", amount)
+#             print("Your Prervious Balance : ", previousBalance)
+#             print("Your  Current Balance  : ", mainBalance)
+#             return mainBalance
+#         else:
+#             print("You don't have enough Balance!")
+#             return mainBalance
         
-#         selectedOption = input("choose your option(example: 2):")
-#         match selectedOption:
-#             case '1':
-#                 print("your balance is: ",mainBalance, "TAKA")
-#             case '2':
-#                 # amount  = int(input("enter an amount (TAKA): "))
-#                 deposite(mainBalance)
-                
-                
-#             case '3':
-#                 # amount  = int(input("enter an amount (TAKA): "))
-#                 withdraw(mainBalance)
-#             case other:
-#                 print("Please enter correct number.")
-#         newBalance = deposite(mainBalance)
-#         return newBalance
-                
-#     # mainBalance =  newBalance
-#     initialPage()  
-    
-   
-    # mainBalance = newBalance
-    
-    
-# Balance = 1600
+              
+
 # i=0
-# while i<10:
-    
-#     mainbalance = ()
-
-#     def deposit(Balance):
-#         amount  = int(input("enter an amount (TAKA): "))
-#         previousBalance = Balance
-#         mainBalance = Balance + amount
-#         print("Deposit amount         : ", amount)
-#         print("Your Prervious Balance : ", previousBalance)
-#         print("Your  Current Balance  : ", mainBalance)
-#         newBalance = mainBalance
-#         return newBalance
-        
-    
-
-    
-#     def withdraw(Balance):
-#         amount = int(input("enter an amount (TAKA): "))
-#         previousBalance = Balance
-#         if(amount<Balance):
-#              mainBalance = Balance - amount
-#         print("withdrawn amount         : ", amount)
-#         print("Your Prervious Balance : ", previousBalance)
-#         print("Your  Current Balance  : ", mainBalance)
-#         newBalance = mainBalance
-#         return newBalance
-    
-#     def initialPage():
-#         print("""
-#                 .-----------------------------------------------.
-#                 |              MAYER DOWYA ATM                  |
-#                 | 1. check balance                              |
-#                 | 2. deposit                                    |
-#                 | 3. withdraw                                   |
-#                 |_______________________________________________|
-                
-#                 """)
-        
-#         selectedOption = input("choose your option(example: 2):")
-#         match selectedOption:
-#             case '1':
-#                 print("your balance is: ",Balance, "TAKA")
-#             case '2':
-#                 # amount  = int(input("enter an amount (TAKA): "))
-#                 deposit(Balance)
-#                 newBalance = mainBalance
-                
-#             case '3':
-#                 # amount  = int(input("enter an amount (TAKA): "))
-#                 withdraw(Balance)
-#                 newBalance = mainBalance
-#             case other:
-#                 print("Please enter correct number.")
-#         newBalance = deposit(Balance)
-#         return newBalance
-                
-# # mainBalance =  newBalance
-#     initialPage()  
-
-
-
-
-# mainBalance = 1600
-# i = 0
-# while i<10:
-
-#  def deposit():
-#     amount = int(input("Enter an amount (TAKA): "))
-#     previousBalance = mainBalance
-#     mainBalance = mainBalance + amount
-#     print("Deposit amount         : ", amount)
-#     print("Your Previous Balance : ", previousBalance)
-#     print("Your Current Balance  : ", mainBalance)
-#     newBalance = mainBalance
-#     return newBalance
-
-#  def withdraw(mainBalance):
-#     amount = int(input("Enter an amount (TAKA): "))
-#     previousBalance = mainBalance
-#     if amount < mainBalance:
-#         mainBalance = mainBalance - amount
-#         print("Withdrawn amount       : ", amount)
-#         print("Your Previous Balance : ", previousBalance)
-#         print("Your Current Balance  : ", mainBalance)
-#         newBalance = mainBalance
-#         return newBalance
-
-#  def updatedBalance(mainBalance):
-    
-#  def initialPage(mainBalance):
+# while i<1:
+#     # userCardNumber = input("Enter your card number : ")
 #     print("""
 #             .-----------------------------------------------.
 #             |              MAYER DOWYA ATM                  |
-#             | 1. Check balance                              |
-#             | 2. Deposit                                    |
-#             | 3. Withdraw                                   |
+#             | 1. check balance                              |
+#             | 2. deposite                                   |
+#             | 3. withdraw                                   |
+#             | 4. Reset Password                             |
 #             |_______________________________________________|
             
-#             .-----------------------------------------------.
-#             |              MAYER DOWYA ATM                  |
-#             | 4.New Balance                                 |
-#             | 5.Exit                                        |
-#             |_______________________________________________|
 #             """)
     
-#     selectedOption = input("Choose your option (example: 2): ")
-#     match selectedOption:
-        
-#         case '1':
-#             print("your main balance is :", mainBalance, "taka" )
-#         case '2':
-#             deposit(mainBalance)
-#             newBalance = mainBalance
-#         case '3':
-#             withdraw(mainBalance)
-#             newBalance = mainBalance
-            
-#             return newBalance
-# initialPage()
+    # selectedOption = input("choose an option : ")
+    # match selectedOption:
+    #     case '1':
+    #         inputPass = input("enter your PIN : ")
+    #         if(inputPass == userPass):
+    #             print("your balance is : ",mainBalance, "TAKA")
+    #         else:
+    #             print("Wrong PIN! Please try again")
+    #     case '2':
+    #         # amount  = int(input("enter an amount (TAKA): "))
+    #         mainBalance = deposite(mainBalance)
+    #     case '3':
+    #         # amount  = int(input("enter an amount (TAKA): "))
+    #         mainBalance =  withdraw(mainBalance)
+    #     case '4':
+    #         inputPass = input("enter your current PIN : ")
+    #         if(inputPass==userPass):
+    #             newPass = input("enter a new PIN : ")
+    #             if(len(newPass) == 4):
+    #                 userPass = newPass
+    #             else:
+    #                 print("Enter 4 digits PIN number.")
+                
+    #     case other:
+    #         print("Please enter correct number.")
+
+
+
 
